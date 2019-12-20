a70feb56d95207d13e656d7e9c6b403463129acc
Gonzalo Sola <gonzalosola@gmail.com>
2019-12-20 12:25:42 -0300

    Reestructura
    Signed-off-by: gsola <gfsola@afip.gob.ar>

--- src/main/java/com/example/vaadin/Home.java
+++ src/main/java/com/example/vaadin/Home.java
@@ -1,11 +1,12 @@
-package com.example.demoVaadin;
+package com.example.vaadin;
 
 import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.router.Route;
 
 @Route("")
-public class HelloWorld extends Div {
-    public HelloWorld() {
+public class Home extends Div {
+    public Home() {
         setText("Hello world");
+
     }
 }
